5782c65992f23d519e5a47c9068ce4a3a4e0cbde
Adrian Barrio <statickidz@gmail.com>
2024-10-13 16:16:53 +0200

    chore: remove root login

--- bin/dokploy-worker.sh
+++ bin/dokploy-worker.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Add ubuntu SSH authorized keys to the root user
-mkdir -p /root/.ssh
-cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/
-chown root:root /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
+# # Add ubuntu SSH authorized keys to the root user
+# mkdir -p /root/.ssh
+# cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/
+# chown root:root /root/.ssh/authorized_keys
+# chmod 600 /root/.ssh/authorized_keys
 
 # Add ubuntu user to sudoers
 echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-# OpenSSH
-apt install openssh-server
-systemctl status sshd
+# # OpenSSH
+# apt install openssh-server
+# systemctl status sshd
 
-# Permit root login
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-systemctl restart sshd
+# # Permit root login
+# sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# systemctl restart sshd
