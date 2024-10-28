3b99a110e8941b25575e6a084915f06f47c445be
Adrian Barrio <statickidz@gmail.com>
2024-10-28 08:22:30 +0100

    chore: update variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,31 +1,31 @@
 variable "ssh_authorized_keys" {
-  description = "SSH public key for instances. For example: ssh-rsa AAEAB3NaC1yc2EAAAA....3xcoeATR ssh-key-2024-09-03"
+  description = "SSH public key for instances. For example: ssh-rsa AAEAAAA....3R ssh-key-2024-09-03"
   type        = string
 }
 
 variable "compartment_id" {
-  description = "The OCID of the compartment. Find it: Profile - Tenancy: youruser - Tenancy information - OCID (copy) https://cloud.oracle.com/tenancy"
+  description = "The OCID of the compartment. Find it: Profile → Tenancy: youruser → Tenancy information → OCID https://cloud.oracle.com/tenancy"
   type        = string
 }
 
 variable "source_image_id" {
-  description = "Source Ubuntu 22.04 image OCID. Find it for your region: https://docs.oracle.com/en-us/iaas/images/image/128dbc42-65a9-4ed0-a2db-be7aa584c726/index.htm. For example: ocid1.image.oc1.phx.aaaaaaaa47msfuw77sfw4eabf23tko6e6kbbbogbeuo6po6ni3lfgjkc5j2q"
+  description = "Source Ubuntu 22.04 image OCID. Find the right one for your region: https://docs.oracle.com/en-us/iaas/images/image/128dbc42-65a9-4ed0-a2db-be7aa584c726/index.htm"
   type        = string
 }
 
 variable "num_worker_instances" {
-  description = "Number of Dokploy worker instances to deploy."
+  description = "Number of Dokploy worker instances to deploy (max 3 for free tier)."
   type        = number
   default     = 1
 }
 
 variable "availability_domain_main" {
-  description = "Availability domain for dokploy-main instance. For example: AaJg:EU-FRANKFURT-1-AD-1"
+  description = "Availability domain for dokploy-main instance. Find it Core Infrastructure → Compute → Instances → Availability domain (left menu). For example: WBJv:EU-FRANKFURT-1-AD-1"
   type        = string
 }
 
 variable "availability_domain_workers" {
-  description = "Availability domain for dokploy-worker instances. For example: AaJg:EU-FRANKFURT-1-AD-2"
+  description = "Availability domain for dokploy-main instance. Find it Core Infrastructure → Compute → Instances → Availability domain (left menu). For example: WBJv:EU-FRANKFURT-1-AD-2"
   type        = string
 }
 
